8bd9b32790dfcfcc2e07a1a25bcdee0277f6bec9
IrulArif <masterirul@gmail.com>
2020-05-02 06:02:27 +0700

    fixing container

--- ui/Container/index.tsx
+++ ui/Container/index.tsx
@@ -1,38 +1,28 @@
 import React from "react";
 import { ScrollViewProps, StyleSheet, ViewStyle } from "react-native";
 import View from "../View";
-import _ from "lodash";
 
 export interface IContainerProps extends ScrollViewProps {
   children?: any;
   scrollRef?: any;
-  KeyboardAvoidingView?: boolean;
 }
 
 export default (props: IContainerProps) => {
-  const { style, scrollRef, KeyboardAvoidingView } = props;
+  const { style, scrollRef } = props;
   const baseStyle: ViewStyle = {
     flexGrow: 1,
   };
   const cstyle = StyleSheet.flatten([baseStyle, style]);
   return (
     <View
-      type={KeyboardAvoidingView != false ? "KeyboardAvoidingView" : "View"}
+      type={"ScrollView"}
+      {...props}
       style={{
         flexGrow: 1,
         flexShrink: 1,
       }}
-    >
-      <View
-        type={"ScrollView"}
-        {...props}
-        style={{
-          flexGrow: 1,
-          flexShrink: 1,
-        }}
-        childRef={scrollRef}
-        contentContainerStyle={cstyle}
-      />
-    </View>
+      childRef={scrollRef}
+      contentContainerStyle={cstyle}
+    />
   );
 };
