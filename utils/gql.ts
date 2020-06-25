58d0e050c285e8a85adb93388500fbc7bcd70fab
karf <masterirul@gmail.com>
2020-06-25 15:11:34 +0700

    fixing

--- utils/gql.ts
+++ utils/gql.ts
@@ -22,16 +22,19 @@ export const queryAll = async (q: string, options?: QueryOptions) => {
   }
 
   try {
-    let url = `${config.backend.protocol}://${config.backend.host}:${config.backend.port}/hasura/v1/graphql`;
+    let url;
 
-    if (config.hasura.host) {
+    if (!!config.backend) {
+      url = `${config.backend.protocol}://${config.backend.host}:${config.backend.port}/hasura/v1/graphql`;
+    }
+    if (!!config.hasura) {
       url = `${config.hasura.host}/v1/graphql`;
     }
-    if (!!config.mode && config.mode === "dev") {
+    if (!!config.mode && config.mode === "dev" && !!config["backend-dev"]) {
       url = `${config["backend-dev"].protocol}://${config["backend-dev"].host}:${config["backend-dev"].port}/hasura/v1/graphql`;
-      if (config["hasura-dev"].host) {
-        url = `${config["hasura-dev"].host}/v1/graphql`;
-      }
+    }
+    if (!!config.mode && config.mode === "dev" && config["hasura-dev"]) {
+      url = `${config["hasura-dev"].host}/v1/graphql`;
     }
     const res: any = await api({
       url,
